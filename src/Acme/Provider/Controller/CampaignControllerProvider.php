2ef1646380a8fd5e8564961882e15d429d7932c3
Khalid Adisendjaja <kh411d@yahoo.com>
2012-07-17 09:25:14 +0700

    front

--- src/Acme/Provider/Controller/CampaignControllerProvider.php
+++ src/Acme/Provider/Controller/CampaignControllerProvider.php
@@ -13,6 +13,8 @@ class CampaignControllerProvider implements ControllerProviderInterface
     {
         $controllers = $app['controllers_factory'];
 		
+		/** Home **/
+
 		$controllers->match('/', function(Request $request) use ($app) {
 		 echo "<pre>"; 
 		 var_dump($app['campaign']->current());
@@ -26,6 +28,194 @@ class CampaignControllerProvider implements ControllerProviderInterface
 		
 		})->method('GET|POST')
 		  ->bind('home');
+
+
+		/** Page **/  
+
+		$controllers->match('/page/{id}', function(Request $request,$id) use ($app) {
+		  
+		  if($page = $app['page']->getById($id)){
+		  	return $app['twig']->render('page.html',array('page' => $page));
+		  }else{
+		  	$app->abort(404, "Page does not exist.");
+		  } 
+		
+		})->method('GET|POST')
+		  ->bind('page');		  
+
+        
+        /** Register **/
+
+		$controllers->match('/register', function(Request $request) use ($app) {
+		
+		//Validation
+		if(!$campaign = $app['campaign']->current()){
+			$app->abort(404,"Page does not exists.");
+		}
+
+		if($campaign['on_judging']){
+		    $data['title'] = "The Winner Announce Soon";
+			$data['text'] = "Sorry! We are on Judging Time for The Campaign.";
+			return $app['twig']->render('notification.html',$data);
+	 	}
+
+	 	$sr = $app['facebook']->getSignedRequest();
+	 	$redirect_url = isset($sr['page']) ? '' : "/register";
+	
+	 
+		 if(!$user = $app['helper.facebook']->getAuthorizedUser(true)){
+		 	return $app->redirect($app['url_generator']->generate('authorize').'?ref='.$redirect_url);
+		 }
+		 
+		 if(!$isFan = $app['helper.facebook']->user_isFan()){
+		 	return $app->redirect($app['url_generator']->generate('likepage').'?ref='.$redirect_url);
+		 }
+		 
+		 if($app['customer']->isRegistered($user['id'])){
+		 	return $app->redirect($app['url_generator']->generate('home'));
+		 }
+
+	 	//Form Builder
+		 $builder = $app['form.factory']->createBuilder('form',$data_default);
+			$form = $builder
+					->add('name', 'text', array(
+						'constraints' => new Assert\NotBlank(),
+						'attr'        => array('placeholder' => 'Isi nama anda'),
+						'label'		  => 'Nama'
+					))
+					->add('email', 'text', array(
+						'constraints' => new Assert\NotBlank(),
+						'attr'        => array('placeholder' => 'Isi email anda'),
+						'label'		  => 'Email'
+					))
+					->add('address', 'textarea', array(
+						'attr'        => array('placeholder' => 'isi alamant'),
+						'label'		  => 'Alamat'
+					))
+					->add('phone', 'text', array(
+						'constraints' => new Assert\NotBlank(),
+						'attr'        => array('placeholder' => 'Isi No Telpon anda'),
+						'label'		  => 'Telepon.'
+					))
+					->getForm();
+    	 
+		if ('POST' === $app['request']->getMethod()) {
+				$form->bindRequest($app['request']);
+				if ($form->isValid()) {
+					 $values = $form->getData();
+					 $values['regdate'] = \date('Y-m-d H:i:s');
+					 $values['fb_uid'] = $user['id'];
+					 $result = $app['customer']->add($values);
+					 
+					return $app->redirect($app['url_generator']->generate('upload'));
+				} else {
+					$form->addError(new FormError('Maaf, silahkan coba kembali.'));
+					return $app->redirect($app['url_generator']->generate('register'));
+				}
+		}
+
+    	 return $app['twig']->render('register.html',array('form'=>$form->createView()));
+		
+		})->method('GET|POST')
+		  ->bind('register');
+		/**/  
+
+
+
+        /** Upload **/
+
+		$controllers->match('/upload', function(Request $request) use ($app) {
+		 //Validation
+		if(!$campaign = $app['campaign']->current()){
+			$app->abort(404,"Page does not exists.");
+		}
+
+		if($campaign['on_judging']){
+		    $data['title'] = "The Winner Announce Soon";
+			$data['text'] = "Sorry! We are on Judging Time for The Campaign.";
+			return $app['twig']->render('notification.html',$data);
+	 	}
+
+	 	if(!$campaign['on_upload']){
+		   	$data['title'] = "Campaign Upload End";
+			$data['text'] = "Sorry! Upload submission for the campaign has just ended";
+			return $app['twig']->render('notification.html',$data);
+	 	}
+
+	 	$sr = $app['facebook']->getSignedRequest();
+	 	$redirect_url = isset($sr['page']) ? '' : "/register";
+	
+	 
+		 if(!$user = $app['helper.facebook']->getAuthorizedUser(true)){
+		 	return $app->redirect($app['url_generator']->generate('authorize').'?ref='.$redirect_url);
+		 }
+		 
+		 if(!$isFan = $app['helper.facebook']->user_isFan()){
+		 	return $app->redirect($app['url_generator']->generate('likepage').'?ref='.$redirect_url);
+		 }
+		 
+		 if(!$app['customer']->isRegistered($user['id'])){
+		 	return $app->redirect($app['url_generator']->generate('register'));
+		 }
+
+    	 
+    	 return $app['twig']->render('upload.html',array());
+		
+		})->method('GET|POST')
+		  ->bind('');
+		/**/  
+
+
+
+        /** xxx **
+
+		$controllers->match('', function(Request $request) use ($app) {
+		
+    	 
+    	 return $app['twig']->render('.html',array());
+		
+		})->method('GET|POST')
+		  ->bind('');
+		/**/  
+
+
+
+        /** xxx **
+
+		$controllers->match('', function(Request $request) use ($app) {
+		
+    	 
+    	 return $app['twig']->render('.html',array());
+		
+		})->method('GET|POST')
+		  ->bind('');
+		/**/  
+
+
+        /** xxx **
+
+		$controllers->match('', function(Request $request) use ($app) {
+		
+    	 
+    	 return $app['twig']->render('.html',array());
+		
+		})->method('GET|POST')
+		  ->bind('');
+		/**/  
+
+
+
+        /** xxx **
+
+		$controllers->match('', function(Request $request) use ($app) {
+		
+    	 
+    	 return $app['twig']->render('.html',array());
+		
+		})->method('GET|POST')
+		  ->bind('');
+		/**/  
+
 
         return $controllers;
     }
